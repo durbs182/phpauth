cc2cd7e4736d9642164252707fb6a555811aaec7
Paul Durbin <pauldurbin69@gmail.com>
2014-09-16 20:21:26 +0100

    Revert "nup"
    This reverts commit 7abfa4f28ae17d2fd1f8e8153dd34f4bf5a0a368.

---
+++ onelabadminphp/callback.php
@@ -0,0 +1,62 @@
+
+<?php
+
+function startsWith($haystack, $needle)
+{
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
+}
+
+include_once 'JWT.php';
+
+$secret = base64_decode('1fTiS2clmPTUlNcpwYzd5i4AEFJ2DEsd8TcUsllmaKQ=');
+ini_set('display_errors', 'On');
+  
+$access_token = "";
+  
+if(isset($_SERVER["HTTP_AUTHORIZATION"]))
+{
+  $bearer = $_SERVER["HTTP_AUTHORIZATION"];
+  
+  $bearerStr = "Bearer ";
+  
+  if(startsWith($bearer, $bearerStr))
+  {
+      $pieces = explode($bearerStr, $bearer);
+  
+      //print_r($pieces[1]);
+      $access_token = $pieces[1];
+  }
+}
+    
+ //die($access_token);
+ 
+if( $access_token != "")
+{
+  try
+  {
+	  $jwt = JWT::decode($access_token,$secret ,true);
+			
+    //var_dump($jwt);
+			
+	  $expiry = $jwt->exp;
+			
+	  setcookie( "access_token", $access_token, $expiry , '', '', true, true);
+    
+	  //header("Location: index.php"); 
+	  die('index.php');
+    
+  }
+  catch(Exception $e)
+  {
+	  echo 'Caught exception: ',  $e->getMessage(), "\n";
+    die("cc");
+  }
+}
+
+header('HTTP/1.1 401 Unauthorized', true, 401);
+die('Unauthorized');
+
+
+ 
+?>
