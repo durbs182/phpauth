05a613df14338370a839da9da1529d1279374242
Paul Durbin <pauldurbin69@gmail.com>
2014-09-23 15:16:26 +0100

    cleanup

--- onelabphp/login.php
+++ onelabphp/login.php
@@ -20,71 +20,28 @@ $_SESSION['state']=$state;
     
     <script src="Scripts/jquery-2.0.2.js"></script>
     
-    <div id="state">
-        <b>State:</b> <?php echo $state; ?>
-    </div>
-    
-    <div id="start" >
-        <img src="icon-user.png" id="draggable" draggable="true" ondragstart="event.dataTransfer.setData('text/plain',null)"/>
-    </div>
-<div>
-    <img src="door.png" class="dropzone"/>
-</div>
-
-<style>
-  #draggable {
-    width: 150px;
-    height: 150px;
-   
-  }
-
-  #state {
-    border: solid;
-    width: 180px;
-    height: 30px;
-    position: fixed;
-    top: 35px;
-    left: 15px;
-    padding-top: 10px;
-    padding-left: 10px;
-  }
-  
-  #start {
-    width: 150px;
-    height: 150px;
-    position: fixed;
-    top: 130px;
-    left: 30px;
-  }
- 
-  .dropzone {
-    position: fixed;
-    top: 30px;
-    left: 300px;
-
-  }
-</style>
 
 <script>
-$(function () {
-            var authorizationUrl = 'http://localhost/WebHostwin/users/oauth/authorize';
-            var client_id = 'codeclient';
-            var redirect_uri = 'https://onelabdemo.azurewebsites.net/onelabadminphp/callback.html';
-            var response_type = "code";
-            var scope = "read write search";
-            var state = "<?php echo $state; ?>" ;
-            
-            var url =
-                authorizationUrl + "?" + 
-                "client_id=" + encodeURI(client_id) + "&" + 
-                "redirect_uri=" + encodeURI(redirect_uri) + "&" + 
-                "response_type=" + encodeURI(response_type) + "&" + 
-                "scope=" + encodeURI(scope) + "&" + 
-                "state=" + encodeURI(state);
-            sessionStorage["state"] = state;
-            window.location = url;
-
-  
+	$(function () {
+		var authorizationUrl = 'http://localhost/WebHostwin/users/oauth/authorize';
+		var client_id = 'codeclient';
+		var redirect_uri = 'https://onelabdemo.azurewebsites.net/onelabphp/callback.html';
+		var response_type = "code";
+		var scope = "read write search";
+		var state = "<?php echo $state; ?>" ;
+		
+		var url =
+			authorizationUrl + "?" + 
+			"client_id=" + encodeURI(client_id) + "&" + 
+			"redirect_uri=" + encodeURI(redirect_uri) + "&" + 
+			"response_type=" + encodeURI(response_type) + "&" + 
+			"scope=" + encodeURI(scope) + "&" + 
+			"state=" + encodeURI(state);
+			
+		console.log(url);
+			
+		sessionStorage["state"] = state;
+		window.location = url;
 </script>
     
 </body>
