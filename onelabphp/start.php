f36f402ffad863ea6ce1a49513a80b38828821c1
Paul Durbin <pauldurbin69@gmail.com>
2014-09-23 20:17:50 +0100

    chnage size

--- onelabphp/start.php
+++ onelabphp/start.php
@@ -61,7 +61,7 @@ else
     <script type='text/javascript' src='scripts/jquery.tablesorter.js'></script>
 	<script type="text/javascript">           $(function () { $("#table001").tablesorter({ sortList: [[0, 0]] }); });</script>
 
-<div align="right">
+<div align="right" height="42" width="42">
     <img src="exit.png" />
  </div>
  
